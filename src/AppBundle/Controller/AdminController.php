dcbf55ed5fae705f48be0f3f79c62cb1fbbd4bb8
Karolis Martusevičius <martuseviciusk@gmail.com>
2017-04-16 20:01:46 +0300

    Added admin navigation links and admin user index page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -22,14 +22,26 @@ class AdminController extends Controller
     }
 
     /**
+     * @Route("/edit", name="admin_user_index")
+     */
+    public function userIndexAction()
+    {
+        // Admin panel: user editing panel
+        // Form to change the following user data:
+        // password, email...
+        return $this->render('AppBundle:Admin:edit_user_index.html.twig', [
+
+        ]);
+    }
+
+    /**
      * @Route("/edit/{id}", name="admin_user_edit")
      */
     public function editUserAction($id)
     {
         // Admin panel: user editing panel
-        // Form to change the following user data:
-        // password, email...
-        return $this->render('AppBundle:Admin:edit_user.html.twig', [
+        // Selecting the user to edit
+        return $this->render('AppBundle:Admin:edit_user_index.html.twig', [
             'id' => $id
         ]);
     }
@@ -42,6 +54,17 @@ class AdminController extends Controller
         // Admin panel: interface for disabling rule violating or other users
         return $this->render('AppBundle:Admin:disable_user.html.twig', [
             'id' => $id
+        ]);
+    }
+
+    /**
+     * @Route("/posts", name="admin_posts_index")
+     */
+    public function postsIndexAction()
+    {
+        // Admin panel: post (Decision) index page
+        return $this->render('AppBundle:Admin:posts_index.html.twig', [
+
         ]);
     }
 
