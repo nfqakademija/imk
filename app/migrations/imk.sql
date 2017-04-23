9abbeedacc4f7a6b6118031c567ff99bf6e76e6f
Mindaugas Petraitis <f_ma@hotmail.co.uk>
2017-04-23 19:34:16 +0300

    db update #2

--- app/migrations/imk.sql
+++ app/migrations/imk.sql
@@ -4,12 +4,12 @@ USE imk;
 
 CREATE TABLE IF NOT EXISTS `Users` ( 
 `userId`            INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`userName`          CHAR(32) CHARACTER SET ascii NOT NULL,
+`userName`          CHAR(32) NOT NULL,
 `password`          CHAR(64) CHARACTER SET ascii NOT NULL,
 `email`             CHAR(64) CHARACTER SET ascii NOT NULL DEFAULT '',
 `language`          CHAR(2) CHARACTER SET ascii NOT NULL DEFAULT 'en',
-`country`           CHAR(2) CHARACTER SET ascii NOT NULL DEFAULT '',
-`city`              CHAR(32) NOT NULL DEFAULT '',
+`countryGeonameId`  INT UNSIGNED DEFAULT NULL,
+`cityGeonameId`     INT UNSIGNED DEFAULT NULL,
 `birthDate`         DATE NOT NULL DEFAULT '2000-01-01',
 `createDate`        INT UNSIGNED NOT NULL DEFAULT '0', # unix timestamp
 `updateDate`        INT UNSIGNED NOT NULL DEFAULT '0', # unix timestamp
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `Polls` (
 `updateDate`        INT UNSIGNED NOT NULL DEFAULT '0', # unix timestamp
 `language`          CHAR(2) CHARACTER SET ascii NOT NULL DEFAULT 'en',
 `title`             VARCHAR(250) NOT NULL DEFAULT '',
-INDEX (authorId,createDate,language),
+INDEX (authorId,createDate,updateDate),
 FOREIGN KEY (authorId) REFERENCES Users (userId) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
@@ -45,10 +45,10 @@ FOREIGN KEY (categoryId) REFERENCES Categories (categoryId) ON UPDATE CASCADE ON
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `PollsOptions` ( 
-`optionId`			INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`optionId`          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `pollId`            INT UNSIGNED NOT NULL,
-`title`				VARCHAR(250) NOT NULL DEFAULT '', # text, link or file
-`votesCount` 		INT UNSIGNED NOT NULL DEFAULT '0',
+`content`           VARCHAR(250) NOT NULL DEFAULT '', # text, link or file
+`votesCount`        INT UNSIGNED NOT NULL DEFAULT '0',
 INDEX (pollId),
 FOREIGN KEY (pollId) REFERENCES Polls (pollId) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
@@ -60,5 +60,16 @@ CREATE TABLE IF NOT EXISTS `PollsVoters` (
 `voterIp`           CHAR(45) CHARACTER SET ascii NOT NULL DEFAULT '', # IPv4/IPv6 address
 `voteOptionId`      INT UNSIGNED NOT NULL,
 INDEX (pollId,voterId),
-FOREIGN KEY (pollId) REFERENCES Polls (pollId) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY (pollId) REFERENCES Polls (pollId) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (voterId) REFERENCES Users (userId) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `UsersCategoriesHits` ( 
+`id`                INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`userId`            INT UNSIGNED NOT NULL,
+`categoryId`        INT UNSIGNED NOT NULL,
+`hitsCount`         INT UNSIGNED NOT NULL, # category/tag click count
+INDEX (userId,categoryId),
+FOREIGN KEY (userId) REFERENCES Users (userId) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (categoryId) REFERENCES Categories (categoryId) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
