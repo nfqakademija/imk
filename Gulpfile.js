0f59c5ff618aac6a28d08ed834944a141a42aef2
Karolis Martusevičius <martuseviciusk@gmail.com>
2017-04-25 16:14:50 +0300

    New post button

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@ var uglify  = require('gulp-uglify');
 var dir = {
     assets: './src/AppBundle/Resources/',
     dist: './web/',
-    npm: './node_modules/',
+    npm: './node_modules/'
 };
 
 gulp.task('sass', function() {
@@ -46,4 +46,8 @@ gulp.task('fonts', function() {
         .pipe(gulp.dest(dir.dist + 'fonts'));
 });
 
-gulp.task('default', ['sass', 'scripts', 'fonts', 'images']);
+gulp.task('watch', function() {
+    gulp.watch(dir.assets + 'style/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'scripts', 'fonts', 'images', 'watch']);
